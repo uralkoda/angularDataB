5aa8f0a4daea9dd1eeb9cbf7fc14bbe5a59a24bf
uralkoda <elghoulhamzaa@gmail.com>
2019-07-18 11:31:41 +0100

    first commit  c
    first commitc

---
+++ src/app/app.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  serverElements = [{type: 'server', name: 'Testserver', content: 'just a test!'}];
+
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: {blueprintName: string, blueprintContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.blueprintName,
+      content: blueprintData.blueprintContent
+    });
+  }
+  onChangeFirst(){
+    this.serverElements[0].name  ='changed!!';
+    console.log("onchangefirst");
+  }
+}
---
+++ src/app/server-element/server-element.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, Input, ViewEncapsulation, OnChanges, SimpleChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewChecked } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrls: ['./server-element.component.css'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class ServerElementComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewChecked, AfterContentInit{
+@ Input('srvElement') element: {type: string, name: string, content: string};
+@ Input() name: string;
+  constructor() { 
+    console.log('constructor called');
+  }  
+  ngOnChanges(changes: SimpleChanges){
+    console.log('ngOnChanges called');
+    console.log(changes);
+  }
+  ngOnInit() {
+    console.log('ngOnInit called');
+  }
+  ngDoCheck(){
+    console.log('ngDoCheck called');
+  }
+  ngAfterContentInit(){
+    console.log('ngAfterContentInit called');
+  }
+  ngAfterContentChecked(){
+    console.log('ngAfterContentChecked called');
+  }
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit called');
+  }
+  ngAfterViewChecked(){
+    console.log('ngAfterViewChecked called');
+  }
+
+}
